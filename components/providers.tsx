7810ec5975c661bb455b5f39ac17290533cb5310
Ruchiket Borse <RuchiketBorse1@gmail.com>
2025-05-26 22:12:07 +0530

    changes

--- components/providers.tsx
+++ components/providers.tsx
@@ -3,7 +3,12 @@ import { createStore, Provider } from "jotai";
 import Overlay from "./overlays/overlay";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	const myStore = createStore();
+	const storeKey = "__jotai_store__";
+	const myStore =
+		typeof window !== "undefined"
+			? (window as any)[storeKey] ||
+			  ((window as any)[storeKey] = createStore())
+			: createStore();
 	return (
 		<Provider store={myStore}>
 			<Overlay />
--- utils/store.ts
+++ utils/store.ts
@@ -1,5 +1,6 @@
 import { atom } from "jotai";
 import { Overlay } from "./types/overlay";
+import { atomWithStorage } from "jotai/utils";
 
 export const overlayAtom = atom<Overlay | null>(null);
 export const uploadedImageAtom = atom<File | null>(null);

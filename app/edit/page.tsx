fbbf68998794807a26e2957ee8a69dab0d9bae70
Ruchiket Borse <RuchiketBorse1@gmail.com>
2025-05-26 22:08:39 +0530

    changes

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -6,10 +6,15 @@ import Texture from "@/components/texture";
 import { uploadedImageAtom } from "@/utils/store";
 import { useAtom } from "jotai";
 import { notFound, useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const EditPage = () => {
 	const [uploadedImage, setUploadedImage] = useAtom(uploadedImageAtom);
 	const router = useRouter();
+
+	useEffect(() => {
+		setUploadedImage((prev) => prev);
+	}, []);
 
 	return (
 		<div className="min-h-screen h-full flex flex-col overflow-x-hidden w-screen text-white bg-gradient-to-t from-accent-900 to-40% to-black relative">
